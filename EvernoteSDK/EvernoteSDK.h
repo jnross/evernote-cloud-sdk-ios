6182c0e7d57028b1ee7d113a3000b69c7dd0d68a
Joseph Ross <joe@junelife.com>
2018-11-26 12:31:34 -0800

    Add framework.

---
+++ EvernoteSDK/EvernoteSDK.h
@@ -0,0 +1,20 @@
+//
+//  EvernoteSDK.h
+//  EvernoteSDK
+//
+//  Created by Joseph Ross on 11/26/18.
+//  Copyright © 2018 Evernote Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EvernoteSDK.
+FOUNDATION_EXPORT double EvernoteSDKVersionNumber;
+
+//! Project version string for EvernoteSDK.
+FOUNDATION_EXPORT const unsigned char EvernoteSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EvernoteSDK/PublicHeader.h>
+
+#import <EvernoteSDK/ENSDK.h>
+
